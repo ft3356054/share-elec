99fc0219b1af32fc0cfa71230a74ff1f94c8d063
ft3356054 <346654970@qq.com>
2021-02-05 15:20:58 +0800

    temp

--- src/main/java/com/sgcc/uap/share/controller/LoginController.java
+++ src/main/java/com/sgcc/uap/share/controller/LoginController.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cloopen.rest.sdk.BodyType;
